4e9bd2984efe3069cd3365614b064db914d1ecac
Julien Marland <juju@MacBook-Pro-de-Julien.local>
2023-11-16 00:00:30 +0100

    first try

---
+++ config.js
@@ -0,0 +1,5 @@
+window.__CONFIG__ = {
+  // url must NOT end with a slash
+  VITE_CORS_PROXY_URL: "https://visionfilm.84p9jcbbb7.workers.dev",
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5MmZjMDQ5ZTFjMDg2ZWJlNGY5MjE1NTUzNWJjZmFmYSIsInN1YiI6IjY1NTU0MmFhZDRmZTA0MDBhYzM2M2I2NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.RMBpEKvpPfUCwgwYNc1J1OkbwSJoBE-J7iyxAvbBEVk"
+};
